0a0a23b861397169ca3dbf17915f1724597d8d65
Monoj5212 <monoj5212@gmail.com>
2025-07-21 21:37:48 +0530

    Add files via upload

---
+++ Assignment_Task1.py
@@ -0,0 +1,20 @@
+'''
+Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+'''
+
+a=int(input("Enter 1st Value:"))
+b=int(input("Enter 2nd Value:"))
+add=a+b
+sub=a-b
+mul=a*b
+div=float(a/b)
+print("Addition:",add)
+print("Subtraction:",sub)
+print("Multiplication:",mul)
+print("Division:",div)
---
+++ Assignment_Task2.py
@@ -0,0 +1,10 @@
+'''
+Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+'''
+
+FirstName=input("Enter your First Name:")
+LastName=input("Enter your Last Name:")
+print("Hello "+FirstName+" "+LastName+"! "+"Welcome to the Python Program.")
